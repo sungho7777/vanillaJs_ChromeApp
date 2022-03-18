518a423e7be778346439711a8aa024aeb9a9e61a
sungho7777 <mzncvmc@gmail.com>
2022-03-18 10:45:27 +0900

    #4.6 Loading Username

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME= "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
-    const username=loginInput.value;
-    localStorage.setItem("username", username);
-    
-    greeting.innerHTML=`Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+
+    painGreeings(username);
 }
 
 function handleLinkClick(event){
@@ -21,3 +21,20 @@ function handleLinkClick(event){
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function painGreeings(username){
+    greeting.innerHTML = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    // show the greetings
+    painGreeings(savedUsername);
+}
